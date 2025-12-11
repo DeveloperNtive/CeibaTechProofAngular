0174b0791bcbfc74cee4ff0a73c8830126ec0778
Oscar Eduardo Montes Perez <joboscarmontes@gmail.com>
2025-12-11 17:55:44 -0500

    (Validators on create-user-component.ts fixed)

--- src/app/feature/users/create-user/create-user.component.ts
+++ src/app/feature/users/create-user/create-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-user',
@@ -16,12 +17,15 @@ export class CreateUserComponent implements OnInit {
   createUserJobError!: ElementRef<HTMLButtonElement>;
 
   form: FormGroup;
-  nameErrorMessage = 'Name is required';
-  jobErrorMessage = 'Job is required';
+  nameErrorMessage = '';
+  jobErrorMessage = '';
 
-  constructor(private readonly router: Router, private renderer: Renderer2) {
-    this.form = new FormGroup({
-      name: new FormControl('', Validators.required),
+  constructor(
+    private readonly router: Router, 
+    private renderer: Renderer2, 
+    private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: new FormControl('', [Validators.required]),
       job: new FormControl('', Validators.required),
     });
   }
@@ -30,26 +34,26 @@ export class CreateUserComponent implements OnInit {
   }
 
   checkButtonAvaliabilty() {
-    if (this.form.valid) {
-      this.createUserNameError.nativeElement.remove();
-      this.createUserJobError.nativeElement.remove();
-      if (this.createUserButton.nativeElement.attributes['disabled']) {
-        this.renderer.removeAttribute(this.createUserButton.nativeElement, 'disabled');
-      }
-    } else {
-      if (
-        this.form.get('name').errors?.required &&
-        (this.form.get('name').touched || this.form.get('name').dirty)
-      ) {
+
+    const nameControl = this.form.get('name');
+    const jobControl = this.form.get('job');
+
+    if (nameControl.invalid && (nameControl.dirty || nameControl.touched)) {
+      if (nameControl.hasError('required')) {
         this.nameErrorMessage = 'Name is required';
       }
-      if (
-        this.form.get('job').errors?.required &&
-        (this.form.get('job').touched || this.form.get('job').dirty)
-      ) {
-        this.jobErrorMessage = 'Job is required';
-      }
-      this.renderer.setAttribute(this.createUserButton.nativeElement, 'disabled', 'true');
+    } else {
+      this.nameErrorMessage = '';
+    }
+
+    if (jobControl && (jobControl.dirty || jobControl.touched) && jobControl.hasError('required')) {
+      this.jobErrorMessage = 'Job is required';
+    } else {
+      this.jobErrorMessage = '';
+    }
+
+    if (this.form.valid) {
+      this.renderer.removeAttribute(this.createUserButton.nativeElement, 'disabled');
     }
   }
 
